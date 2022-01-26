0907595bdc0dae359519b0de9cff1eba0417bbda
mille <mille@Notebook-Miller>
2022-01-26 00:27:50 -0200

    Comitando Selenium WebDriver

---
+++ Meus_Estudos_CursoSelenium/src/main/java/TesteGoogle.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TesteGoogle {
+	
+	public static void main(String[] args) {
+//	System.setProperty("webdriver.gecko.driver", "/C/drivers_curso/geckodriver.exe");
+	WebDriver driver = new FirefoxDriver();
+	driver.get("http://www.google.com");
+	System.out.println(driver.getTitle());
+		
+	}
+}
