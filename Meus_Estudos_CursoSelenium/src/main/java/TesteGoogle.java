d2d620ee4149e0de7655c5656e6190645f249724
mille <mille@Notebook-Miller>
2022-01-26 00:50:29 -0200

    .

--- Meus_Estudos_CursoSelenium/src/main/java/TesteGoogle.java
+++ Meus_Estudos_CursoSelenium/src/main/java/TesteGoogle.java
@@ -1,13 +1,15 @@
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TesteGoogle {
 	
-	public static void main(String[] args) {
-//	System.setProperty("webdriver.gecko.driver", "/C/drivers_curso/geckodriver.exe");
-	WebDriver driver = new FirefoxDriver();
-	driver.get("http://www.google.com");
-	System.out.println(driver.getTitle());
+	@Test
+	public static void teste() {
+	//	System.setProperty("webdriver.gecko.driver", "/C/drivers_curso/geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://www.google.com");
+		System.out.println(driver.getTitle());
 		
 	}
 }
