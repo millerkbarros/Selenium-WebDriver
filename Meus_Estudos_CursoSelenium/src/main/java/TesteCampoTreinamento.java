c9f48caad03d7243f89a4566446430a440ca4834
mille <mille@Notebook-Miller>
2022-01-30 01:57:15 -0200

    Comitando meus projetos

--- Meus_Estudos_CursoSelenium/src/main/java/TesteCampoTreinamento.java
+++ Meus_Estudos_CursoSelenium/src/main/java/TesteCampoTreinamento.java
@@ -1,6 +1,5 @@
 import java.awt.Button;
 import java.util.concurrent.TimeUnit;
-
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.validator.PublicClassValidator;
@@ -11,7 +10,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.interactions.ClickAction;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 
 public class TesteCampoTreinamento {
@@ -21,22 +20,48 @@ public class TesteCampoTreinamento {
 //	comando para digitar em um determinado campo
 //	comando para verificar o que está digitado	
 	@Test
-	public void testeTextField() {
+	public void testeTextField(){
 		WebDriver driver = new ChromeDriver();
-		driver.manage().window().setSize(new Dimension(1296, 800));
-		driver.get("https://wejump-automation-test.github.io/qa-test/");
-		driver.findElement(By.id("first_name")).sendKeys("Miller");
-		Assert.assertEquals("Miller", driver.findElement(By.id("first_name")).getAttribute("value"));
-		
+		driver.manage().window().setSize(new Dimension(1200, 765));
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		driver.findElement(By.id("elementosForm:nome")).sendKeys("Miller Cardoso Barros");
+		Assert.assertEquals("Miller Cardoso Barros", driver.findElement(By.id("elementosForm:nome")).getAttribute("value"));
 		driver.quit();
 	}
 	
 	@Test
-	public void deveInteragirComTextAreo() {
+	public void deveInteragirComTextAreo(){
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://wejump-automation-test.github.io/qa-test/");
-		driver.findElement(By.id("mid_name")).sendKeys("Cardoso");
-		Assert.assertEquals("Cardoso", driver.findElement(By.id("mid_name")).getAttribute("value"));
+		driver.manage().window().setSize(new Dimension(1200, 765));
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		driver.findElement(By.id("elementosForm:sugestoes")).sendKeys("campo sugestão");
+		Assert.assertEquals("campo sugestão", driver.findElement(By.id("elementosForm:sugestoes")).getAttribute("value"));
+		driver.quit();
+	}
+
+	@Test
+	public void deveInteragirComRadioButton(){
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().setSize(new Dimension(1200, 765));
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		driver.findElement(By.id("elementosForm:sexo")).click();
+		Assert.assertTrue(driver.findElement(By.id("elementosForm:sexo")).isSelected());
+		driver.quit();
 		
 	}
+
+	@Test
+	public void deveInteragirComCheckBox(){
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().setSize(new Dimension(1200, 765));
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		driver.findElement(By.id("elementosForm:comidaFavorita:2")).click();
+		Assert.assertTrue(driver.findElement(By.id("elementosForm:comidaFavorita:2")).isSelected());
+		driver.quit();
+		
+		
+		
+		
+	}
+
 }
